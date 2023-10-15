c8044f6eb450444c7b5aef199ff600d78bc07a85
Miika Anundi <manundi@gmail.com>
2023-10-15 11:20:21 +0300

    Added moving user inputs left and right

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,49 @@ import 'pixi-spine' // Do this once at the very start of your code. This registe
 import * as PIXI from 'pixi.js'
 import { Spine } from 'pixi-spine'
 import { Text, Container } from 'pixi.js'
- 
-type Vector2 ={
+
+type Vector2 = {
     x: number,
     y: number
 }
 
 type CharacterSettings = {
-    name : string,
-    scale : number,
-    speed : number,
-
+    name: string,
+    scale: number,
+    speed: number,
 }
 
-const screenSize : Vector2 = {x:window.innerWidth, y:window.innerHeight}
+const screenSize: Vector2 = { x: window.innerWidth, y: window.innerHeight }
 
 //we have an a character, we will call it player
-const playerSettings : CharacterSettings = {
+const playerSettings: CharacterSettings = {
     name: 'Captain Cosmic',
     scale: 0.3,
-    speed: 1
+    speed: 2
 }
 
 
+const playerTransform = new Container()
+const playerNamePlate = new Text(playerSettings.name)
+let animation: any
+let gameHasStarted = false
+
+
+let userInput = {
+    isInput: false,
+    x: 0,
+    y: 0
+}
+
+let player = {
+    transform: playerTransform,
+    animation: animation,
+    namePlate: playerNamePlate,
+    velocity: 1,
+}
+
 // define basic settings of html canvas. Sizer bg color etc.
-const game = new PIXI.Application<HTMLCanvasElement>({
+const game = new PIXI.Application<any>({
     width: screenSize.x,
     height: screenSize.y,
     backgroundColor: 'pink',
@@ -37,32 +55,98 @@ document.body.appendChild(game.view)
 
 PIXI.Assets.load('/assets/animation/spineboy-pro.json').then((resource) => {
     //console.log('Loaded spineboy: ', resource)
-    const playerTransform = new Container()
-	const animation = new Spine(resource.spineData);
-    const charNamePlate = new Text(playerSettings.name)
-    charNamePlate.setTransform(-150,1,2,2)
-    charNamePlate.setParent(playerTransform)
+
+    animation = new Spine(resource.spineData);
+    playerNamePlate.setTransform(-250, 1, 3, 3)
+    playerNamePlate.setParent(playerTransform)
     animation.setParent(playerTransform)
 
     game.stage.addChild(playerTransform);
-    
+
     // lets move the newly created animation to center of the screen
 
     playerTransform.setTransform(
-        screenSize.x /2,
-        screenSize.y/2 + animation.height*playerSettings.scale/2,
-        playerSettings.scale*1,playerSettings.scale*1
+        screenSize.x / 2,
+        screenSize.y / 2 + animation.height * playerSettings.scale / 2,
+        playerSettings.scale * 1, playerSettings.scale * 1
     )
-    
 
-    
     if (animation.state.hasAnimation('run')) {
         animation.state.setAnimation(0, 'run', true);
-        animation.state.timeScale = 0.1;
+        animation.state.timeScale = 0.1 * playerSettings.speed;
         animation.autoUpdate = true;
     }
 });
 
+const GameLoop = () => {
+    //initial animation /movement when user did not yet take control
+    if (!gameHasStarted) {
+        playerTransform.position.set(
+            playerTransform.position.x + playerSettings.speed,
+            playerTransform.position.y)
+    //user has control, game has started
+    } else {
+        if (userInput.isInput) {
+            
+        } else {
+            player.velocity = Lerp(player.velocity, 0, 0.03)
+        }
+
+        animation.state.timeScale =  0.1 *  Math.abs(player.velocity)
+        playerTransform.position.set(
+        playerTransform.position.x + player.velocity,
+        playerTransform.position.y)
+      
+    }
+    if (playerTransform.x > screenSize.x + playerTransform.width / 2) {
+        playerTransform.x = -playerTransform.width / 2;
+    }
+    if (playerTransform.x < -100 ) {
+        playerTransform.x = screenSize.x +20
+    }
+    //TODO: Fix moving to idle when playervelocity is near 0
+    //if(animation?.state.timeScale  <0.1) animation.state.setAnimation(0, 'run-to-idle', false)
+
+}
+
+game.ticker.add(GameLoop)
+game.ticker.start()
+
+const Lerp = (a: number, b: number, t: number) => {
+    let result = (1 - t) * a + t * b;
+    //threshold to stop compute if close to target
+    if (Math.abs(result - b) < 0.1) {
+        return 0;
+    }
+    return result;
+}
+
+
+document.addEventListener('keydown', (e) => {
+    userInput.isInput = true
+    //TODO: this does not need to set everytime, how to prevent?
+    gameHasStarted = true
+    switch (e.key) {
+        case 'ArrowRight':
+        case 'd':
+            player.velocity = playerSettings.speed
+            animation.scale.x = 1
+            break;
+
+        case 'ArrowLeft':
+        case 'a':
+            player.velocity = -playerSettings.speed
+            animation.scale.x = -1
+            break;
+
+        default:
+            break;
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    userInput.isInput = false
+});
 
 
 
