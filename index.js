284a3ce8d7f8ddbf4d61421ce4eb35e535908ba4
manundi <manundi@gmail.com>
2023-10-24 10:33:00 +0300

    port 80

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', async(req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(8080, () => {
+app.listen(80, () => {
     console.log("Sderver successfully running on port 8080");
   });
